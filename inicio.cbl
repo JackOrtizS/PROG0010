f46350be9e4efb761abe90cd7b422b20ea07246c
JackOrtizS <osjack01@gmail.com>
2024-05-13 17:27:16 -0600

    Creacion de proyecto

---
+++ inicio.cbl
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. "INICIO".
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello Jack, Welcome to COBOL"
+            STOP RUN.
+       END PROGRAM "INICIO".
